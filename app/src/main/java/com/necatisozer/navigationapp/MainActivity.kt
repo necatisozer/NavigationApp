19b4ad3cffcbc6edcf96a7a6d60aeba9a6f2f391
necati <necatisozer@gmail.com>
2021-08-02 21:25:10 +0300

    Add toolbar

--- app/src/main/java/com/necatisozer/navigationapp/MainActivity.kt
+++ app/src/main/java/com/necatisozer/navigationapp/MainActivity.kt
@@ -1,10 +1,21 @@
 package com.necatisozer.navigationapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
     }
 }
